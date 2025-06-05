1f89d0a39077d933a500e02eba7764fee726f742
hpebben <hpebben@users.noreply.github.com>
2025-06-05 17:14:02 +0200

    Wire up wizard block

--- src/edit.js
+++ src/edit.js
@@ -7,14 +7,17 @@
 
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
+import Wizard from './components/wizard';
 
-export default function Edit() {
+export default function Edit( props ) {
     const blockProps = useBlockProps();
 
     return (
-        <div { ...blockProps } style={ { padding: '1rem', border: '1px solid #ccc' } }>
-            <h2>{ __( 'EndoPlanner Wizard (Editor)', 'endoplanner' ) }</h2>
-            <p>{ __( 'Replace this area with your multi-step React wizard.', 'endoplanner' ) }</p>
+        <div { ...blockProps }>
+            <Wizard
+                blockAttributes={ props.attributes }
+                setAttributes={ props.setAttributes }
+            />
         </div>
     );
 }
