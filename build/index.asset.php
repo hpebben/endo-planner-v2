8bbedfc1296c8c2c2525b381f30826bc809570b6
hpebben <hpebben@users.noreply.github.com>
2025-06-09 16:22:36 +0200

    style: center wizard container

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '535ab0afa80873953f05');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cacc378f25ea5a78d3eb');
