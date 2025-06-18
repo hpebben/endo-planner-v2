ee49b7722cded28c126cdb81ca0fe53fea473c79
hpebben <hpebben@users.noreply.github.com>
2025-06-18 18:41:46 +0200

    chore: bump plugin version to v1.6.43

--- endo-planner.php
+++ endo-planner.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: EndoPlanner 2.0
  * Description: A Gutenberg‐based wizard for clinical indication, patency mapping, case summary, intervention planning, and PDF export.
- * Version:     1.6.42
+ * Version:     1.6.43
  * Author:      hpebben
  * License:     GPL2+
  *
