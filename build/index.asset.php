e4930debf6dcb2e5e080466d7c1f76f8bf316bc7
hpebben <hpebben@users.noreply.github.com>
2025-06-18 15:43:56 +0200

    build: update compiled assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8b0bdf851b1e69c44563');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a97791fed1f7f3df8bee');
