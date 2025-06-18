4da8789bbee56a17fc4164a454f58c142ac69e46
hpebben <hpebben@users.noreply.github.com>
2025-06-18 20:56:27 +0200

    fix: improve VesselMap wiring and remove debug overlay

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cdbaa86a55553bdd2cbb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a3364cf1ceb560bac1a2');
