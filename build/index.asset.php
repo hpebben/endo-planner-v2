84c6a801634083fa5399942e4f94b2856f0ed7d5
hpebben <hpebben@users.noreply.github.com>
2025-06-18 16:46:24 +0200

    fix: handle svg interactions with hover and selection

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a97791fed1f7f3df8bee');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1c3453df6fbc21ef4271');
