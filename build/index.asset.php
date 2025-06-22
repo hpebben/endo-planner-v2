f4970295aac4aeff451b945373e8e90827fe074a
hpebben <hpebben@users.noreply.github.com>
2025-06-22 12:39:16 +0200

    fix: handle vessel segment JSON variations

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b6063068b592d2f6d17c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'beaa46569da24b064c0e');
