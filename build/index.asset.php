4056c7b30c5f2b94cab7507e2b41b193e5c24211
hpebben <hpebben@users.noreply.github.com>
2025-06-22 16:39:05 +0200

    feat: update patency controls

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cdbc81287a8901390bdd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '736b8a51eabd174b845e');
