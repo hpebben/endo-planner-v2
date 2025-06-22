92d5f88b2291a782621cda4cd1f3bc1d61b34b7e
hpebben <hpebben@users.noreply.github.com>
2025-06-22 16:26:32 +0200

    feat: add vessel segment parameter popup

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8ee1344ea278d891c808');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cdbc81287a8901390bdd');
