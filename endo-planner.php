f22351f68d65b8de96686e396ec4cd7c95bbd51d
hpebben <hpebben@users.noreply.github.com>
2025-06-22 10:29:24 +0200

    chore: bump plugin version to v1.6.65

--- endo-planner.php
+++ endo-planner.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Endo Planner v2
  * Description:     EndoPlanner Wizard block
- * Version:         1.6.64
+ * Version:         1.6.65
  * Author:          hpebben
  * Text Domain:     endoplanner
  */
