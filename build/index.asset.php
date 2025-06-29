68e34da57b43386d24dd48cfe5ee136317f1a76c
hpebben <hpebben@users.noreply.github.com>
2025-06-29 09:18:36 +0200

    feat: update selector button styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '18da57d9ddd371918ec7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '42b216ddb53ca8ad6d40');
