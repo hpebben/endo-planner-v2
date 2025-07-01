1f25d3092662cd239e32f38c01fdadc6b675ddd9
hpebben <hpebben@users.noreply.github.com>
2025-07-01 22:14:12 +0200

    fix: enable popup interaction

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'fe75792c8d00e7d3f8b4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '595d43b478f3fbfdfb4d');
