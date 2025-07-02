ca878f9b14f03da0c7d3f3130cc142c8cb65842e
hpebben <hpebben@users.noreply.github.com>
2025-07-02 08:35:55 +0200

    feat: improve intervention planning UI

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2664c6a5f2bef325b01d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7e975b8c90375e7a1de6');
