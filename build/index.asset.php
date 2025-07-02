3dba574010bc3dc28aef8a46dc6a10d1409ae346
hpebben <hpebben@users.noreply.github.com>
2025-07-02 08:48:16 +0200

    fix therapy row labels

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2664c6a5f2bef325b01d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '73be40c7951ce8e5029d');
