ec48e4017c83818f9442c61b1d46c4522c56c803
hpebben <hpebben@users.noreply.github.com>
2025-07-02 17:47:58 +0200

    feat: refine interventional planning UI

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a8d5bda38071975f8827');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '32e4270f85ee7c7d62a9');
