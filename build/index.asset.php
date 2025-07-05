a67dfd5ff449f86c7697eb8edceee3bce1762f6f
hpebben <hpebben@users.noreply.github.com>
2025-07-05 22:05:43 +0200

    refactor: enhance case summary details

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1ac37d86901efbefe360');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3af2745f2e08c380c7df');
