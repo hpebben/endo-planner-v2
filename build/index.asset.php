c1c0e617ef4d020077c1bd01fd0dd9690fb6bab6
hpebben <hpebben@users.noreply.github.com>
2025-07-07 21:45:57 +0200

    feat: switch pdf export to html template

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd0d8aea0fd74951e30e9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'fa026e11164827d564d5');
