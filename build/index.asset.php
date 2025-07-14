39121b1a6416895a0355dd502f3c27df8e8b0af6
hpebben <hpebben@users.noreply.github.com>
2025-07-14 20:31:22 +0200

    fix: remove unused react-router dependency

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3150191c02ae4175947e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '88c56046f0c78684c7f3');
