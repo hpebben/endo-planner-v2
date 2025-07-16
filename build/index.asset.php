c89aa28cea3b65474a0274a5dee0731d726e9689
hpebben <hpebben@users.noreply.github.com>
2025-07-16 10:16:02 +0200

    fix: omit shaft length in therapy summary

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1f72b02c809b2589212b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '22d32c2e7dadff53f120');
