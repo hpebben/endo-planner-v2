a9851b0a6394862d43564050e7abe3c1bc133bd6
thesisappscom <thesisappscom@tb-nl01-linssh002.srv.teamblue-ops.net>
2026-01-08 18:32:36 +0100

    chore: bump plugin version to v1.6.163

--- endo-planner.php
+++ endo-planner.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Endo Planner v2
  * Description:     EndoPlanner Wizard block
- * Version:         1.6.162
+ * Version:         1.6.163
  * Author:          hpebben
  * Text Domain:     endoplanner
  */
