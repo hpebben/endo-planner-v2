7e937a801c55ac381a8a111eda2bb8cf630db413
hpebben <hpebben@users.noreply.github.com>
2026-08-02 17:48:42 +0200

    Fix React runtime in browser tests

--- src/components/wizard.jsx
+++ src/components/wizard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import DEFAULTS from './Defaults';
 import exportCaseSummaryToPDF from '../utils/exportCaseSummaryToPDF';
 import { __ } from '@wordpress/i18n';
