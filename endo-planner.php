e6cdd259c2510fb4b7c37c105d3f0b6c1a8716ba
hpebben <hpebben@users.noreply.github.com>
2026-08-02 13:26:45 +0200

    Bump plugin version to 1.6.165

--- endo-planner.php
+++ endo-planner.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Endo Planner v2
  * Description:     EndoPlanner Wizard block + Intervention Planning frontend
- * Version:         1.6.164
+ * Version:         1.6.165
  * Author:          hpebben
  * Text Domain:     endoplanner
  */
